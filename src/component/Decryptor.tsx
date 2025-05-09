3d80547c8668c251ebc54f65dcc7e77d7a1ed457
Teepat17 <asiaasdf719@gmail.com>
2025-05-09 14:53:00 +0700

    update game

--- src/component/Decryptor.tsx
+++ src/component/Decryptor.tsx
@@ -18,8 +18,8 @@ const MiniGame = ({ onWin, onClose }: GameProps) => {
 
     const moveStar = () => {
       setStarPosition({
-        x: Math.random() * 100,
-        y: Math.random() * 100
+        x: Math.random() * 500,
+        y: Math.random() * 500
       });
     };
 
